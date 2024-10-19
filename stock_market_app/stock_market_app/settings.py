421a4881d5e0e2e10b744d732e3f3042575efb2a
Akash Pawar <akashs.pawar222@gmail.com>
2024-10-19 16:02:41 +0530

    Update settings.py

--- stock_market_app/stock_market_app/settings.py
+++ stock_market_app/stock_market_app/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z6-^diqak@8zh3jt8sncwa8%12@&fn$5u7$d)=v@*kr0=!j7bb'
+SECRET_KEY = ########
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
